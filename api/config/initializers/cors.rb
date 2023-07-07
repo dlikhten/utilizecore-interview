3b0b33ea47be1fc73594123e4b48cf5843523ad8
Dima <dlikhten@gmail.com>
2023-07-06 23:18:37 -0400

    fixing cors

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -8,5 +8,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins /localhost:*/, /127\.0\.0\.1:*/, /192\.168\.0\.150:*/
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
